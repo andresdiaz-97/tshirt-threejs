86291ce4db5a6099df735464e47c146628ff7a85
andresdiaz-97 <andres.diaz1508@gmail.com>
2024-02-10 23:54:03 -0300

    created folder structure and working with the initial screen, h1, paragraph and button

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  return (
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}>
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
