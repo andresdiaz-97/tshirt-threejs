5512531fcd7dcf85db3d5de88cd56110f3ef97e5
andresdiaz-97 <andres.diaz1508@gmail.com>
2024-02-13 23:47:15 -0300

    started working on the camera rig

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,7 +1,27 @@
-import React from "react"
+import React, { useRef } from "react"
+import { useFrame } from "@react-three/fiber"
+import { easing } from "maath"
+import { useSnapshot } from "valtio"
+import state from "../store"
 
-const CameraRig = () => {
-  return <div>CameraRig</div>
+const CameraRig = ({ children }) => {
+  const group = useRef()
+  const snap = useSnapshot(state)
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260
+    const isMobile = window.innerWidth <= 600
+
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      0.1
+    )
+  })
+
+  return <group ref={group}>{children}</group>
 }
 
 export default CameraRig
